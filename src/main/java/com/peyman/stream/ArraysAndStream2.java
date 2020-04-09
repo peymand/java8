879976bc643bc8b9f481da2a833931fc05f2f66c
Peymandodangeh@gmail.com <asemanEABI@@90>
2020-04-09 15:29:33 +0430

    String arrays and arraylist in streams

---
+++ src/main/java/com/peyman/stream/ArraysAndStream2.java
@@ -0,0 +1,24 @@
+package com.peyman.stream;
+
+import java.util.Arrays;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class ArraysAndStream2 {
+    public static void main(String[] args) {
+        String[] strings = {"Red", "orange", "yellow", "green", "blue", "indigo", "Violet"};
+
+        System.out.printf("Original strings: %s%n", Arrays.asList(strings));
+
+        System.out.printf("strings in uppercase: %s%n", Arrays.stream(strings).map(String::toUpperCase)
+                .collect(Collectors.toList()));
+        //strings greater than 'm' sorted ascending
+        Arrays.stream(strings).filter(s->s.compareToIgnoreCase("m")>0)
+                .sorted(String.CASE_INSENSITIVE_ORDER)
+                .collect(Collectors.toList());
+        //strings greater than 'm' sorted descending
+        Arrays.stream(strings).filter(s->s.compareToIgnoreCase("m")>0)
+                .sorted(String.CASE_INSENSITIVE_ORDER.reversed())
+                .collect(Collectors.toList());
+    }
+}
